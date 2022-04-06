d897bd77406a2ad58fd5554c2a25b7c0b8174f7f
KonstantinosKoulaxizis <kkoulaxizis07@gmail.com>
2022-04-06 18:55:06 +0200

    chore: Fix ReactDOM warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
@@ -7,13 +7,14 @@ import reportWebVitals from './reportWebVitals'
 
 import './index.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
